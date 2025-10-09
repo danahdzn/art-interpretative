31b42a64f2cb6611bf034f35e58009b7fe680aa8
Dana Berenice Hernandez Norberto <danaahdzn@ciencias.unam.mx>
2025-10-09 15:10:07 -0600

    Ya

--- ArtInterpretative.py
+++ ArtInterpretative.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import pandas as pd
 import base64
 from math import sqrt
+import io
 from transformers import pipeline, BlipProcessor, BlipForConditionalGeneration, AutoTokenizer, AutoModelForCausalLM
 import hashlib
 
